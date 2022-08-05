578cfa25a6f6873a1ce884cdfde0712e15046487
JoaoAugustoMV <joaootusugua@gmail.com>
2022-08-05 16:52:36 -0300

    ExerciciosMap

--- Java/Colecoes/src/Map/ExercicioDados.java
+++ Java/Colecoes/src/Map/ExercicioDados.java
@@ -1,9 +1,23 @@
 package Map;
 
+import java.util.Map;
+import java.util.Random;
+import java.util.TreeMap;
+
 public class ExercicioDados {
+    public static void main(String[] args) {
+
+        Map<Integer, Integer> nums = new TreeMap<>();
+        Random rand= new Random();
+        for(int i = 0; i != 10; i++){
+            nums.put(i, rand.nextInt(100));
+        }
+        System.out.println(nums);
+    }
     
 }
 
 
 
 // Lance um dado 100 e mostre quantas vezes caiu cada valor
+// chave o numero e valor será a contagem
--- Java/Colecoes/src/Map/ExercicioEstadosBr.java
+++ Java/Colecoes/src/Map/ExercicioEstadosBr.java
@@ -6,8 +6,11 @@
  */
  package Map;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 class ExercicioEstadosBr{
     public static void main(String[] args) {
@@ -33,26 +36,48 @@ class ExercicioEstadosBr{
         // for (Map.Entry<String, Integer> estado: estadosBr.entrySet()) {
         //     if
         // }
-    }
- }
+
+        // Exiba a população de PE
+        System.out.println("A populacao de PE é de " + estadosBr.get("PE") + " pessoas");
+        
+        // Exiba o dicionario na ordem alfabetica(por key)
+        Map<String, Integer> estadosBrTree = new TreeMap<>(estadosBr);
+        System.out.println("1 " + estadosBrTree);
+        
+         // Estado com menor população
+        Integer menorPopulacao = Collections.min(estadosBr.values());
+        String estadoMenorPopulacao = getKey(estadosBr, menorPopulacao);
+        System.out.println("O Estado com a menor populacao é " + estadoMenorPopulacao);
+        // Estado com maior população
+        Integer maiorPopulacao = Collections.max(estadosBr.values());
+        String estadoMaiorPopulacao = getKey(estadosBr, maiorPopulacao);
+        System.out.println("O Estado com a maior populacao é " + estadoMaiorPopulacao);
+
+        // Soma das populações
+        Integer soma = 0;
+        for (Map.Entry<String, Integer> estado : estadosBr.entrySet()) {
+            soma = soma +  estado.getValue();
+        }
+        System.out.println("A soma das populacoes é " + soma);
+        // Médias das populações
+        Integer media = soma / estadosBr.size();
+        System.out.println("A media das populacoes é " + media);
+        
+        // Remover estados com populações menores que 4.000.000
+        estadosBr.entrySet().removeIf(item -> item.getValue() < 4000000);
+        System.out.println(estadosBr);
+        
+        // Apague o dicionario
+        estadosBr.clear();
+    } // end Main
+    static String getKey(Map<String, Integer> mapa, Integer valor){
+        for (Map.Entry<String, Integer> item: mapa.entrySet()) {
+            if(item.getValue() == valor){
+                return item.getKey();
+            }
+        };
+        return null;
+    } 
+}
 
 
-
-
- // Exiba a população de PE
-
- // Exiba o dicionario na ordem de inserção
-
- // Exiba o dicionario na ordem alfabetica
-
- // Estado com menor população
-
- // Estado com maior população
-
- // Soma das populações
-
- // Médias das populações
-
- // Remover estados com populações menores que 4.000.000
-
- // Apague o dicionario
