ca3937a6ace1dba913511b72debbfe4f252c557f
JoaoAugustoMV <joaootusugua@gmail.com>
2022-08-08 13:02:37 -0300

    Exercicios

--- Java/Colecoes/src/Map/ExercicioDados.java
+++ Java/Colecoes/src/Map/ExercicioDados.java
@@ -1,19 +1,36 @@
 package Map;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.TreeMap;
 
 public class ExercicioDados {
     public static void main(String[] args) {
-
-        Map<Integer, Integer> nums = new TreeMap<>();
         Random rand= new Random();
-        for(int i = 0; i != 10; i++){
-            nums.put(i, rand.nextInt(100));
+
+        List<Integer> numsSorteados = new ArrayList<>();
+        for(int i = 1; i <= 10; i++){ // Faz sorteio dos numeros
+            numsSorteados.add(rand.nextInt(1, 7));
         }
-        System.out.println(nums);
-    }
+
+        Map<Integer, Integer> mapCont = new TreeMap<>();
+        
+        for (Integer i : numsSorteados) { // Itera o elemento a ser contado 
+            int contador = 0;
+            for (Integer n: numsSorteados) { // Itera a lista para a contagem
+                if(n == i){ // Se o elemento a ser contado for igual ao iterador
+                    contador++;
+                }
+            } // end for(n)
+            mapCont.put(i, contador);
+        } // end for(i)
+        
+        for (Map.Entry<Integer, Integer> entry: mapCont.entrySet()) {
+            System.out.println(entry.getKey() + " apareceu " + entry.getValue() + " vezes");
+        }
+    }// end main()
     
 }
 
